2daa813f35c3e6215f4ff07b1821e6359c4501f4
kgc42 <kgc42@cornell.edu>
2025-08-21 00:00:42 -0400

    fixed filepaths

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, Date, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from backend.app.database.db import Base
+from app.database.db import Base
 from datetime import datetime
 
 class User(Base):
